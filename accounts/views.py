f569132650cdc84513ffcc844a5663f685e931de
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-20 11:24:20 +0330

    create a simple class view for account

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
+class LoginView(TemplateView):
+    template_name = "pages/login.html"
