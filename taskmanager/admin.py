ed28ebe8ab751bb2d1b722a1f41c619772b2b13a
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-25 17:27:50 +0330

    register task on admin

--- taskmanager/admin.py
+++ taskmanager/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "user",
+        "status",
+        "created_at",
+        "updated_at",
+    )  # Fields to display in the list view
+    list_filter = ("status", "created_at", "updated_at")  # Filters for the list view
+    search_fields = ("title", "user__username")  # Search functionality
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )  # Make these fields read-only in the admin panel
