390e687c06199f989a852a2f43f999019dc0ad5e
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-25 12:43:47 +0330

    create view for task-list

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
+class TaskListView(TemplateView):
+    template_name = "pages/task-list.html"
