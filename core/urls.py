1fe7485828c4324d3f985082bb37db6121127f8c
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-25 12:41:51 +0330

    uncomment task urls

--- core/urls.py
+++ core/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
-    # path("task", include("taskmanager.urls")),
+    path("task/", include("taskmanager.urls")),
 ]
 
 if settings.DEBUG:
