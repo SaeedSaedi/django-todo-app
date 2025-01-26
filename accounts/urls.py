3fdba09cc63c729ef906e9793687da882d821c73
saeed saedi <saeed.saedi.s@gmail.com>
2025-01-26 11:28:59 +0330

    create logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from . import views
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 app_name = "accounts"
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path("", views.LoginView.as_view(), name="login"),
     path("register/", views.RegisterView.as_view(), name="register"),
     path("forget-password/", views.ForgetPasswordView.as_view(), name="forget-password"),
+    path('logout/', LogoutView.as_view(next_page='accounts:login'), name='logout'),
 ]
