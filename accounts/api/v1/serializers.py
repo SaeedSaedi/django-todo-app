83c40b1eac6a135dc00e27fd7c39e00e190cead4
saeed saedi <saeed.saedi.s@gmail.com>
2025-02-04 12:27:22 +0330

    create serializer for register api

---
+++ accounts/api/v1/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ("email", "first_name", "last_name", "password", "password2")
+
+    def validate(self, data):
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError({"password": "Passwords must match."})
+        return data
+
+    def create(self, validated_data):
+        validated_data["username"] = validated_data["email"]
+        validated_data.pop("password2")  # Remove password2 as it's not in the model
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
