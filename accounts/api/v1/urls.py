5e0f3ece83baf1faaf9f4224a22ca334c9730454
saeed saedi <saeed.saedi.s@gmail.com>
2025-02-09 12:42:08 +0330

    create route for jwt login,create,refresh and verify

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
-from .views import RegisterAPIView, LoginAPIView, LogoutAPIView, PasswordResetAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from .views import (
+    RegisterAPIView,
+    LoginAPIView,
+    LogoutAPIView,
+    PasswordResetAPIView,
+    JWTAuthLoginAPIView,
+)
 
 app_name = "accounts_api"
 
@@ -8,4 +19,9 @@ urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
     path("logout/", LogoutAPIView.as_view(), name="logout"),
     path("password-reset/", PasswordResetAPIView.as_view(), name="password_reset"),
+    # JWT Authentication URLs
+    path("jwt/login/", JWTAuthLoginAPIView.as_view(), name="jwt_login"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
